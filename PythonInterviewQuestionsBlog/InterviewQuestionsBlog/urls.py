5a16dac5864cbfd66992fb83449aaccf3c330aaf
sagarreddyg <gantasagarreddy467@gmail.com>
2018-07-28 19:57:19 +0530

    W in 2nd Question

--- PythonInterviewQuestionsBlog/InterviewQuestionsBlog/urls.py
+++ PythonInterviewQuestionsBlog/InterviewQuestionsBlog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 app_name = 'InterviewQuestionsBlog'
 urlpatterns = [
-    url(r'^IV/$', views.PythonInterViewQuestions.as_view(), name='questions'),
+    url(r'^/$', views.PythonInterViewQuestions.as_view(), name='questions'),
 
 ]
--- PythonInterviewQuestionsBlog/PythonInterviewQuestionsBlog/urls.py
+++ PythonInterviewQuestionsBlog/PythonInterviewQuestionsBlog/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^InterviewQuestions/', include('InterviewQuestionsBlog.urls', namespace='InterView')),
+    url(r'^$/', include('InterviewQuestionsBlog.urls', namespace='InterView')),
 ]
